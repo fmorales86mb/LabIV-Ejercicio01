7726ef65b949f3523c8820f3ebf744ac0ec8c6f0
fmorales-mobile <fmorales@mobilecomputing.com.ar>
2021-03-18 23:32:46 -0300

    Se resuelve Ejercicio1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ejercicio01';
+  edad1:number = 0;
+  edad2:number = 0;
+  promedio:number = 0;
+  suma:number = 0;
+
+  private SetSuma():void{
+    this.suma = this.edad1 + this.edad2;
+  }
+
+  private SetPromedio():void{
+    if(this.suma != 0){
+      this.promedio = this.suma / 2;
+    }else{
+      this.promedio = 0;
+    }
+  }
+
+  public ClickCalcular():void{
+    this.SetSuma();
+    this.SetPromedio();
+  }
+
+  public ClickLimpiar():void{
+    this.suma = 0;
+    this.promedio = 0;
+    this.edad1 = 0;
+    this.edad2 = 0;
+  }
 }
