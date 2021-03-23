32369882cb5a30620a1573d560d0e9f2d9810627
fm <fm>
2021-03-23 12:21:53 -0300

    Add components bienvenido, error, login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './pages/bienvenido/bienvenido.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
